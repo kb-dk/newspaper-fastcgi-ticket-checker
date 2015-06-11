b0c167e1d1539b5d289f1e84f5436e47568537da
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-06-11 09:30:38 +0200

    Fixed indention

--- fcgid-access-checker/CheckTicket.pm
+++ fcgid-access-checker/CheckTicket.pm
@@ -30,64 +30,64 @@ sub returnStatusCodeFor {
     my ($json, $ticket, $remote_ip, $requested_resource, $resource_type) = @_;
 
     if (!defined $json) {
-	print STDERR "no json\n";
-	return $INTERNAL_SERVER_ERROR;
+        print STDERR "no json\n";
+        return $INTERNAL_SERVER_ERROR;
     }
 
     if (!defined $ticket) {
-	print STDERR "no ticket\n";
-	return $INTERNAL_SERVER_ERROR;
+        print STDERR "no ticket\n";
+        return $INTERNAL_SERVER_ERROR;
     }
 
     if (!defined $remote_ip) {
-	print STDERR "no remote_ip\n";
-	return $INTERNAL_SERVER_ERROR;
+        print STDERR "no remote_ip\n";
+        return $INTERNAL_SERVER_ERROR;
     }
 
     if (!defined $requested_resource) {
-	print STDERR "no requested_resource\n";
-	return $INTERNAL_SERVER_ERROR;
+        print STDERR "no requested_resource\n";
+        return $INTERNAL_SERVER_ERROR;
     }
     
     if (!defined $resource_type) {
-	print STDERR "no resource_type\n";
-	return $INTERNAL_SERVER_ERROR;
+        print STDERR "no resource_type\n";
+        return $INTERNAL_SERVER_ERROR;
     }
 
     my $json_ticket;
 
     # http://docstore.mik.ua/orelly/perl/cookbook/ch10_13.htm
     eval { # decode fails very hard, avoid crashing.
-	$json_ticket = $json->decode($ticket);
+    	$json_ticket = $json->decode($ticket);
     };
 
     if ($@) {
-	print STDERR "decode croaked - most likely bad JSON\n";
-	return $INTERNAL_SERVER_ERROR;
+	    print STDERR "decode croaked - most likely bad JSON\n";
+	    return $INTERNAL_SERVER_ERROR;
     }
 
     if (!defined $json_ticket) {
         print STDERR "no json_ticket\n";
-	return $INTERNAL_SERVER_ERROR;
+	    return $INTERNAL_SERVER_ERROR;
     }
 
     # -- Check IP number is defined and correct.
     
     if (!defined $remote_ip) {
         print STDERR "bad remote_ip\n";
-	return $INTERNAL_SERVER_ERROR;
+	    return $INTERNAL_SERVER_ERROR;
     }
 
     my $json_ticket_ipaddress = $json_ticket->{ipAddress};
 
     if (!defined $json_ticket_ipaddress) {
         print STDERR "bad json_ticket_ipaddress\n";
-	return $INTERNAL_SERVER_ERROR;
+	    return $INTERNAL_SERVER_ERROR;
     }
     
     if ($remote_ip ne $json_ticket_ipaddress) {
-	print STDERR "IP different: " . $remote_ip . " != " . $json_ticket_ipaddress . "\n";
-	return $FORBIDDEN; 
+	    print STDERR "IP different: " . $remote_ip . " != " . $json_ticket_ipaddress . "\n";
+	    return $FORBIDDEN;
     }
     
     # -- Check requested UUID is in list of resources inside ticket.
@@ -97,37 +97,37 @@ sub returnStatusCodeFor {
 
     if (@resources) {
 
-	# Fail if the requested resource uuid is not one of the resources described in the ticket.
-	# doms_radioTVCollection:uuid:853a0b31-c944-44a5-8e42-bc9b5bc697be
-	
-	my $found = $FALSE;
-	
-	# (Note: Array regexp requires perl 5.10 and is experimental,
-	# so not used.)
-	
-	for my $resource (@resources) {
-	    # http://stackoverflow.com/a/6640851/53897
-	    $resource =~ m/uuid:([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$/;
-	    if ($1 eq $requested_resource) {
-		$found = $TRUE;
-		last;
+        # Fail if the requested resource uuid is not one of the resources described in the ticket.
+        # doms_radioTVCollection:uuid:853a0b31-c944-44a5-8e42-bc9b5bc697be
+
+        my $found = $FALSE;
+
+        # (Note: Array regexp requires perl 5.10 and is experimental,
+        # so not used.)
+
+        for my $resource (@resources) {
+            # http://stackoverflow.com/a/6640851/53897
+            $resource =~ m/uuid:([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$/;
+            if ($1 eq $requested_resource) {
+                $found = $TRUE;
+                last;
+            }
+        }
+        if ($found == $FALSE) {
+            print STDERR "bad resource: $requested_resource not in \[@resources\]\n";
+            return $FORBIDDEN;
 	    }
-	}
-	if ($found == $FALSE) {
-	    print STDERR "bad resource: $requested_resource not in \[@resources\]\n";
-	    return $FORBIDDEN; 
-	}
     } else {
-	print STDERR "bad memcached entry\n";
-	return $INTERNAL_SERVER_ERROR;
+        print STDERR "bad memcached entry\n";
+        return $INTERNAL_SERVER_ERROR;
     }
     
     # -- Check content type
 
     if ($resource_type ne $json_ticket->{type}) {
         print STDERR "different media requested than approved for: resource_type $resource_type != json_ticket{type} " . $json_ticket->{type} . "\n";
-	return $BAD_MEDIA_TYPE;
-    }      
+	    return $BAD_MEDIA_TYPE;
+    }
 
     # -- Nothing left to check. We're good.
     
