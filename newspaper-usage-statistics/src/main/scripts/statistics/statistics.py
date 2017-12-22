6e08c08c86d8aa7c91f3eb67956b33d2d14ab159
tra <tra@kb.dk>
2017-12-22 04:18:02 +0100

    statistics: chunkified log file reading.  Handle midnight logfile rollover.
    Logfiles at callisto were found to roll over at midnight so the logfile stamped
    2012-12-12 contained log entries from 2012-12-11 and the triggering log entry after
    midnight.  Therfore we consider one logfile more to get all entries for that day.

--- newspaper-usage-statistics/src/main/scripts/statistics/statistics.py
+++ newspaper-usage-statistics/src/main/scripts/statistics/statistics.py
@@ -30,8 +30,6 @@ import time
 from io import BytesIO
 from lxml import etree as ET
 
-#
-
 config_file_name = "../../newspaper_statistics.py.cfg"  # outside web root.
 
 encoding = "utf-8"  # What to use for output
@@ -86,15 +84,18 @@ def ObjectCache(**kw):
 suds.client.ObjectCache = ObjectCache
 mediestream_webservice = suds.client.Client(mediestream_wsdl)
 
-# -- extract and setup
+# -- extract configuration and setup
 
 if "type" in parameters:
     requiredType = parameters["type"]
 else:
-    requiredType = ""
+    # We cannot generically ask for any type in batch.
+    raise ValueError("'type' must be a parameter.")
 
-# Note:  Dates are handled as strings on the form YYYY-MM-DD.  This allows simple
-# lexographic comparisons.
+if "chunksize" in parameters:
+    chunksize = int(parameters["chunksize"])
+else:
+    raise ValueError("'chunksize' (maximum size of summa request) must be a numeric parameter.")
 
 if "fromDate" in parameters:
     start_str = parameters["fromDate"]  # "2013-06-15"
@@ -121,6 +122,8 @@ namespaces = {
     "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
     "dc": "http://purl.org/dc/elements/1.1/"
 }
+
+downloadPDF = requiredType == "Download"
 
 # -- go
 
@@ -158,16 +161,21 @@ for statistics_file_name in sorted(glob.iglob(statistics_file_pattern)):
     # thumbnails.log
     # thumbnails.log.2017-10-30.gz
     # thumbnails.log.2017-10-31
+    # They are rolled over at midnight so contains the previous days log.
 
     if not os.path.isfile(statistics_file_name):
         continue
 
     # Only process filenames with a YYYY-MM-DD date if they are in range.
+    # Todays log was rolled over at midnight at the first entry of today,
+    # and contains yestedays entries so we need an additional day
+
     filenameDateMatch = re.search(r"\d\d\d\d-\d\d-\d\d", statistics_file_name)
     if filenameDateMatch:
-        filename_date = datetime.date.fromtimestamp(time.mktime(time.strptime(filenameDateMatch.group() + " 10:00", '%Y-%m-%d %H:%M')))
-        if not start_date <= filename_date <= end_date:
-            # print("Skipping %s" % statistics_file_name)
+        filename_date = datetime.date.fromtimestamp(
+            time.mktime(time.strptime(filenameDateMatch.group() + " 10:00", '%Y-%m-%d %H:%M')))
+
+        if not start_date <= filename_date <= (end_date + datetime.timedelta(days=1)):
             continue
 
     # For now just skip compressed files.
@@ -176,149 +184,168 @@ for statistics_file_name in sorted(glob.iglob(statistics_file_pattern)):
     else:
         statistics_file = open(statistics_file_name, "rb")
 
-    for line in statistics_file:
-
-        # Mon Jun 22 15:28:02 2015: {"resource_id":"...","remote_ip":"...","userAttributes":{...},"dateTime":1434979682,"ticket_id":"...","resource_type":"Download"}
-
-        lineParts = line.partition(": ")
-
-        loggedJson = lineParts[2]
-
-        try:
-            entry = json.loads(loggedJson)
-        except:
-            print("Bad JSON skipped from ", statistics_file_name, ": ", loggedJson, file=sys.stderr)
-            continue
-
-        # -- line to be considered?
-
-        entryDate = datetime.date.fromtimestamp(entry["dateTime"])
-
-        if not start_date <= entryDate <= end_date:
-            continue
-
-        if requiredType != "" and not requiredType == entry["resource_type"]:
-            continue
-
-        resource_id = entry["resource_id"]
-
-        downloadPDF = entry["resource_type"] == "Download"
-
-        # -- only process each ticket/domsID once (deep zoom makes _many_ requests).
-
-        uniqueID = resource_id + " " + entry["ticket_id"] + " " + str(downloadPDF)
-
-        if uniqueID in previously_seen_uniqueID:
-            continue
-        else:
-            previously_seen_uniqueID.add(uniqueID)
-
-        # -- ask summa for additional information (with a cache)
-
-        summa_resource_cache_key = resource_id + " " + str(downloadPDF)
-
-        if summa_resource_cache_key in summa_resource_cache:
-            summa_resource = summa_resource_cache[summa_resource_cache_key]
-        else:
-            if downloadPDF:
-                # noinspection PyDictCreation
-                query = {}
-                query["search.document.query"] = "editionUUID:\"doms_aviser_edition:uuid:" + resource_id + "\""
-                query["search.document.maxrecords"] = "20"
-                query["search.document.startindex"] = "0"
-                query["search.document.resultfields"] = "pageUUID, shortformat, familyId"
-                query["solrparam.facet"] = "false"
-                query["group"] = "true"
-                query["group.field"] = "editionUUID"
-                query["search.document.collectdocids"] = "false"
+    # Read the file in chunks of "chunksize" lines and make a single Summa request for each.
+    eof_seen = False
+    while not eof_seen:
+        chunk = []
+        while len(chunk) < chunksize:
+            line = statistics_file.readline()
+            if line:
+                chunk.append(line)
             else:
+                eof_seen = True
+                break
+
+        # Here we have at most chunksize lines (fewer if at end of this log file)
+
+        if len(chunk) > 0:
+            for line in chunk:
+                # Mon Jun 22 15:28:02 2015: {"resource_id":"...","remote_ip":"...","userAttributes":{...},"dateTime":1434979682,"ticket_id":"...","resource_type":"Download"}
+
+                lineParts = line.partition(": ")
+
+                loggedJson = lineParts[2]
+
+                try:
+                    entry = json.loads(loggedJson)
+                except:
+                    print("Bad JSON skipped from ", statistics_file_name, ": ", loggedJson, file=sys.stderr)
+                    continue
+
+                # -- line to be considered?
+
+                entryDate = datetime.date.fromtimestamp(entry["dateTime"])
+
+                if not start_date <= entryDate <= end_date:
+                    continue
+
+                if requiredType != "" and not requiredType == entry["resource_type"]:
+                    continue
+
+                resource_id = entry["resource_id"]
+
+                # -- only process each ticket/domsID once (deep zoom makes _many_ requests).
+
+                uniqueID = resource_id + " " + entry["ticket_id"] + " " + str(downloadPDF)
+
+                if uniqueID in previously_seen_uniqueID:
+                    continue
+                else:
+                    previously_seen_uniqueID.add(uniqueID)
+
+                # -- ask summa for additional information (with a cache)
+
+                summa_resource_cache_key = resource_id + " " + str(downloadPDF)
+
+                if summa_resource_cache_key in summa_resource_cache:
+                    summa_resource = summa_resource_cache[summa_resource_cache_key]
+                else:
+                    query = {}
+                    if downloadPDF:
+                        query["search.document.query"] = "editionUUID:\"doms_aviser_edition:uuid:" + resource_id + "\""
+                        query["search.document.maxrecords"] = "20"
+                        query["search.document.startindex"] = "0"
+                        query["search.document.resultfields"] = "pageUUID, shortformat, familyId"
+                        query["solrparam.facet"] = "false"
+                        query["group"] = "true"
+                        query["group.field"] = "editionUUID"
+                        query["search.document.collectdocids"] = "false"
+                    else:
+                        query["search.document.query"] = "pageUUID:\"doms_aviser_page:uuid:" + resource_id + "\""
+                        query["search.document.maxrecords"] = "20"
+                        query["search.document.startindex"] = "0"
+                        query["search.document.resultfields"] = "pageUUID, shortformat, familyId"
+                        query["solrparam.facet"] = "false"
+                        query["group"] = "true"
+                        query["group.field"] = "pageUUID"
+                        query["search.document.collectdocids"] = "false"
+
+                    queryJSON = json.dumps(query)
+                    # FIXME:  May time out.  Handle that gracefully.
+                    summa_resource_text = mediestream_webservice.service.directJSON(queryJSON)
+                    # print(summa_resource_text.encode(encoding))
+
+                    summa_resource = ET.parse(BytesIO(bytes(bytearray(summa_resource_text, encoding='utf-8'))))
+                    summa_resource_cache[summa_resource_cache_key] = summa_resource
+
+                    # for very large log files the cache needs to be emptied once in a while.
+                    if len(summa_resource_cache) > summa_resource_cache_max:
+                        summa_resource_cache = {}
+
+                # --
+
+                try:
+                    shortFormat = summa_resource.xpath(
+                        "/responsecollection/response/documentresult/group/record/field[@name='shortformat']/shortrecord")[
+                        0]
+                except:
+                    shortFormat = ET.Element("empty")
+
+                # -- ready to generate output
+
                 # noinspection PyDictCreation
-                query = {}
-                query["search.document.query"] = "pageUUID:\"doms_aviser_page:uuid:" + resource_id + "\""
-                query["search.document.maxrecords"] = "20"
-                query["search.document.startindex"] = "0"
-                query["search.document.resultfields"] = "pageUUID, shortformat, familyId"
-                query["solrparam.facet"] = "false"
-                query["group"] = "true"
-                query["group.field"] = "pageUUID"
-                query["search.document.collectdocids"] = "false"
+                outputLine = {}
 
-            queryJSON = json.dumps(query)
-            # FIXME:  May time out.  Handle that gracefully.
-            summa_resource_text = mediestream_webservice.service.directJSON(queryJSON)
-            # print(summa_resource_text.encode(encoding))
+                outputLine["Type"] = "info:fedora/doms:Newspaper_Collection"
 
-            summa_resource = ET.parse(BytesIO(bytes(bytearray(summa_resource_text, encoding='utf-8'))))
-            summa_resource_cache[summa_resource_cache_key] = summa_resource
+                outputLine["Adgangstype"] = entry["resource_type"]
 
-            # for very large log files the cache needs to be emptied once in a while.
-            if len(summa_resource_cache) > summa_resource_cache_max:
-                summa_resource_cache = {}
+                outputLine["UUID"] = resource_id
 
-        # --
+                outputLine["Timestamp"] = datetime.datetime.fromtimestamp(entry["dateTime"]).strftime(
+                    "%Y-%m-%d %H:%M:%S")
 
-        try:
-            shortFormat = summa_resource.xpath(
-                "/responsecollection/response/documentresult/group/record/field[@name='shortformat']/shortrecord")[0]
-        except:
-            shortFormat = ET.Element("empty")
+                outputLine["Klient"] = "-"  # disabled to conform to logging law - was:  entry["remote_ip"]
 
-        # -- ready to generate output
+                # print(ET.tostring(shortFormat))
+                outputLine["AvisID"] = (summa_resource.xpath(
+                    "/responsecollection/response/documentresult/group/record[1]/field[@name='familyId']/text()") or [
+                                            ""])[0]
 
-        # noinspection PyDictCreation
-        outputLine = {}
+                outputLine["Avis"] = \
+                    (shortFormat.xpath("rdf:RDF/rdf:Description/newspaperTitle/text()", namespaces=namespaces) or [""])[
+                        0]
 
-        outputLine["Type"] = "info:fedora/doms:Newspaper_Collection"
+                outputLine["Udgivelsestidspunkt"] = \
+                    (shortFormat.xpath("rdf:RDF/rdf:Description/dateTime/text()", namespaces=namespaces) or [""])[0]
 
-        outputLine["Adgangstype"] = entry["resource_type"]
+                outputLine["Udgivelsesnummer"] = \
+                    (shortFormat.xpath("rdf:RDF/rdf:Description/newspaperEdition/text()", namespaces=namespaces) or [
+                        ""])[0]
 
-        outputLine["UUID"] = resource_id
+                outputLine["schacHomeOrganization"] = ", ".join(
+                    e for e in entry["userAttributes"].get("schacHomeOrganization", {}))
 
-        outputLine["Timestamp"] = datetime.datetime.fromtimestamp(entry["dateTime"]).strftime("%Y-%m-%d %H:%M:%S")
+                outputLine["eduPersonPrimaryAffiliation"] = ", ".join(
+                    e for e in entry["userAttributes"].get("eduPersonPrimaryAffiliation", {}))
 
-        outputLine["Klient"] = "-"  # disabled to conform to logging law - was:  entry["remote_ip"]
+                outputLine["eduPersonScopedAffiliation"] = ", ".join(
+                    e for e in entry["userAttributes"].get("eduPersonScopedAffiliation", {}))
 
-        # print(ET.tostring(shortFormat))
-        outputLine["AvisID"] = (summa_resource.xpath(
-            "/responsecollection/response/documentresult/group/record[1]/field[@name='familyId']/text()") or [""])[0]
+                outputLine["eduPersonPrincipalName"] = ", ".join(
+                    e for e in entry["userAttributes"].get("eduPersonPrincipalName", {}))
 
-        outputLine["Avis"] = \
-            (shortFormat.xpath("rdf:RDF/rdf:Description/newspaperTitle/text()", namespaces=namespaces) or [""])[0]
+                outputLine["eduPersonTargetedID"] = ", ".join(
+                    e for e in entry["userAttributes"].get("eduPersonTargetedID", {}))
 
-        outputLine["Udgivelsestidspunkt"] = \
-            (shortFormat.xpath("rdf:RDF/rdf:Description/dateTime/text()", namespaces=namespaces) or [""])[0]
+                outputLine["SBIPRoleMapper"] = ", ".join(e for e in entry["userAttributes"].get("SBIPRoleMapper", {}))
 
-        outputLine["Udgivelsesnummer"] = \
-            (shortFormat.xpath("rdf:RDF/rdf:Description/newspaperEdition/text()", namespaces=namespaces) or [""])[0]
+                outputLine["MediestreamFullAccess"] = ", ".join(
+                    e for e in entry["userAttributes"].get("MediestreamFullAccess", {}))
 
-        outputLine["schacHomeOrganization"] = ", ".join(
-            e for e in entry["userAttributes"].get("schacHomeOrganization", {}))
+                if not downloadPDF:
+                    # Does not make sense on editions
+                    outputLine["Sektion"] = \
+                        (shortFormat.xpath("rdf:RDF/rdf:Description/newspaperSection/text()",
+                                           namespaces=namespaces) or [""])[0]
+                    outputLine["Sidenummer"] = \
+                        (shortFormat.xpath("rdf:RDF/rdf:Description/newspaperPage/text()", namespaces=namespaces) or [
+                            ""])[0]
 
-        outputLine["eduPersonPrimaryAffiliation"] = ", ".join(
-            e for e in entry["userAttributes"].get("eduPersonPrimaryAffiliation", {}))
-
-        outputLine["eduPersonScopedAffiliation"] = ", ".join(
-            e for e in entry["userAttributes"].get("eduPersonScopedAffiliation", {}))
-
-        outputLine["eduPersonPrincipalName"] = ", ".join(
-            e for e in entry["userAttributes"].get("eduPersonPrincipalName", {}))
-
-        outputLine["eduPersonTargetedID"] = ", ".join(e for e in entry["userAttributes"].get("eduPersonTargetedID", {}))
-
-        outputLine["SBIPRoleMapper"] = ", ".join(e for e in entry["userAttributes"].get("SBIPRoleMapper", {}))
-
-        outputLine["MediestreamFullAccess"] = ", ".join(
-            e for e in entry["userAttributes"].get("MediestreamFullAccess", {}))
-
-        if not downloadPDF:
-            # Does not make sense on editions
-            outputLine["Sektion"] = \
-                (shortFormat.xpath("rdf:RDF/rdf:Description/newspaperSection/text()", namespaces=namespaces) or [""])[0]
-            outputLine["Sidenummer"] = \
-                (shortFormat.xpath("rdf:RDF/rdf:Description/newspaperPage/text()", namespaces=namespaces) or [""])[0]
-
-        encodedOutputLine = dict((key, outputLine[key].encode(encoding)) for key in outputLine.keys())
-        result_dict_writer.writerow(encodedOutputLine)
+                encodedOutputLine = dict((key, outputLine[key].encode(encoding)) for key in outputLine.keys())
+                result_dict_writer.writerow(encodedOutputLine)
 
     statistics_file.close()
     # result_file.close() - can't on sys.stdout.
+
+# ---
