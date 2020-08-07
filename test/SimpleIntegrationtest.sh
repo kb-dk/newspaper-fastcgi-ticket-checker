2e564cd6838a3e09be7b02b0fc87edf4567121cc
Kim Christensen <ktc@kb.dk>
2020-08-07 13:48:02 +0200

    Remember to use port 8080, fix subshell rather than substitution

--- test/SimpleIntegrationtest.sh
+++ test/SimpleIntegrationtest.sh
@@ -2,12 +2,12 @@
 
 PROJECT=$1
 
-BASE_URL="http://image-server.$PROJECT..svc.cluster.local"
+BASE_URL="http://image-server.$PROJECT.svc.cluster.local:8080"
 
 
-curl -sS -w '{http_code}\n' -o /tmp/content $BASE_URL/tv-thumbnail/371157ee-b120-4504-bfaf-364c15a4137c?ticket=3d2bda8b-8b7c-47e9-85ce-f42d2e4fc12d > /tmp/out
+curl -sS -w '%{http_code}' -o /tmp/content $BASE_URL/tv-thumbnail/371157ee-b120-4504-bfaf-364c15a4137c?ticket=3d2bda8b-8b7c-47e9-85ce-f42d2e4fc12d > /tmp/out
 
-HTTP_CODE=${cat /tmp/out}
+HTTP_CODE=$(cat /tmp/out)
 
 echo $HTTP_CODE
 
