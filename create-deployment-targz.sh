80eee2e2005c9ac26836f5d577445de7d2921eaf
Toke Eskildsen <toes@kb.dk>
2023-06-29 13:29:48 +0200

    Harden version extraction for release script

--- create-deployment-targz.sh
+++ create-deployment-targz.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DIR=newspaper-fastcgi-ticket-checker-$(head -n 1 CHANGELOG.md)
+DIR=newspaper-fastcgi-ticket-checker-$(grep -m 1 -o "^[0-9.]\+" CHANGELOG.md)
 
 mkdir -p tmp
 tar cvzf tmp/${DIR}.tgz "--transform=s_^_${DIR}/_" CHANGELOG.md fcgid-access-checker/
